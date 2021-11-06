d7159575f3fe42a0fab22d4f192fb2ff15ef039e
Fernando Reyes <ferdreyes20@gmail.com>
2021-11-06 20:34:05 +0000

    FE - Added login logic to get user

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../_models/user';
+import { UserParams } from '../_models/userParams';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    let user: UserParams;
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      let username = params['username'];
+      let password = params['password'];
+
+      user = {
+        username,
+        password
+      } as UserParams;
+    });
+
+    this.http.post("api/Login/AuthUser", user).subscribe(
+      (response: User) => {
+        console.log({ "response": response });
+        localStorage.setItem("ferdsUser", JSON.stringify(response));
+      },
+      (errors) => {
+        console.log({ "errors": errors });
+      }
+    );
+  }
 }
