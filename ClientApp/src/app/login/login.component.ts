129997aa7840ffbd71dc38b5456f519527aad267
Fernando Reyes <ferdreyes20@gmail.com>
2021-11-07 00:31:21 +0000

    FE - remove dubugger

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -31,7 +31,6 @@ export class LoginComponent implements OnInit {
         localStorage.setItem("ferdsUser", JSON.stringify(response));
         localStorage.setItem("ferdsJwt", response.token);
 
-        debugger;
         this.router.navigate(["/"]);
       },
       (errors) => {
