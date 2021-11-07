e4f6a1f909d01730b55bc65b45b35a7cae1477d6
Fernando Reyes <ferdreyes20@gmail.com>
2021-11-07 05:32:26 +0000

    FE - Fix design of list of territories

--- ClientApp/src/app/_components/list/list.component.ts
+++ ClientApp/src/app/_components/list/list.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 import { Territory } from "src/app/_models/territory";
 
 @Component({
   selector: 'list',
   templateUrl: './list.component.html',
+  styleUrls: ["list.component.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class ListComponent implements OnInit {
   @Input() territories: Territory[] = [];
