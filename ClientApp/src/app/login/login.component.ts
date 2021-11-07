c0d12e70c7158e36d2e1f49d7dd6403bbaadb1e9
Fernando Reyes <ferdreyes20@gmail.com>
2021-11-07 02:45:54 +0000

    FE - Added form UI for Login page

--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from '../_models/user';
 import { UserParams } from '../_models/userParams';
@@ -9,32 +11,39 @@ import { UserParams } from '../_models/userParams';
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  public user: UserParams;
+  public errors: any;
 
   constructor(private httpClient: HttpClient, private router: Router ,private activatedRoute: ActivatedRoute) {
   }
   ngOnInit(): void {
-    let user: UserParams;
+    const currentUser = localStorage.getItem("ferdsUser");
+    if (currentUser) {
+      this.router.navigate(["/"]);
+    }
+  }
 
-    this.activatedRoute.queryParams.subscribe(params => {
-      let username = params['username'];
-      let password = params['password'];
+  login(form: NgForm) {
 
-      user = {
-        username,
-        password
-      } as UserParams;
-    });
+    this.user = {
+      username : form.value.username,
+      password :form.value.password
+    } as UserParams;
 
-    this.httpClient.post("api/Login/AuthUser", user).subscribe(
+    this.httpClient.post("api/Login/AuthUser", this.user).subscribe(
       (response: User) => {
-        console.log({ "response": response });
         localStorage.setItem("ferdsUser", JSON.stringify(response));
         localStorage.setItem("ferdsJwt", response.token);
 
         this.router.navigate(["/"]);
       },
       (errors) => {
-        console.log({ "errors": errors });
+        if(errors.error.errors) {
+          this.errors = errors.error.errors;
+        } else {
+          this.errors = 
+            JSON.parse(errors.error.error);
+        }
       }
     );
   }
